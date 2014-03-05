268e6d40588e8bd1b39a0a81975a63daba368e81
Joel A. Bair <joel.a.bair@gmail.com>
2014-03-05 15:08:23 -0500

    support schemaless dotsub urls

--- plugins/domains/dotsub.com.js
+++ plugins/domains/dotsub.com.js
@@ -5,11 +5,27 @@ module.exports = {
         "canonical",
         "oembed-site",
         "oembed-author",
-
-        "oembed-video-responsive",        
+        "oembed-duration",
+        "oembed-description",
+        "oembed-video-responsive",
         "og-image",
         "favicon"
     ],
+
+    getLink: function(oembed) {
+
+        var rx = /(?:(?:http[s]?:)?\/\/(?:.*\.)?)dotsub\.com\/(?:view|media)\/([^\/\"\'\?#&\s\[\]\(\)]+)/i;
+        var m = rx.exec(oembed.html);
+        if (m && m[1]) {
+            oembed.video_id = m[1];
+            return [{
+                href: "//dotsub.com/media/" + oembed.video_id + '/embed/eng',
+                type: CONFIG.T.text_html,
+                rel: CONFIG.R.player,
+                "aspect-ratio": oembed.width / oembed.height
+            }];
+        }
+    },
 
     tests: [
         "http://dotsub.com/view/6c5d7514-5656-476a-9504-07dd4e2f6509"
